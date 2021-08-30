c4e671fe62024ac50cd82a5d9b5e78f0da4a9e95
tholuongduc <tholuongduc@live.com>
2021-08-31 00:08:11 +0700

    Day 12: BT Vẽ hình theo yêu cầu

--- BT_Ve_Hinh_theo_yeu_cau.py
+++ BT_Ve_Hinh_theo_yeu_cau.py
@@ -6,12 +6,14 @@
 #* Số lượng hình muốn vẽ (quay đều các hình được vẽ theo tâm đường tròn).
 #* Màu sắc (Người dùng có thể chọn màu ngẫu nhiên hoặc màu cụ thể: đỏ, vàng, xanh dương, xanh lá cây, tím,...)
 #Kết quả: Các hình được vẽ trên màn hình theo đúng kích thước, số lượng, khối hình, màu sắc đã chọn.
+
 import random
 import turtle
 import Draw_module
 
 #Define Input function
 def input_cubes():
+    print("Please choose your cubes (circle, square, triangle, rectangle, oval, pentagon, heart):")
     cubes_list = ['circle', 'square', 'triangle', 'rectangle', 'oval', 'pentagon', 'heart']
     while True:
             cubes = input("Please enter your cubes:")
@@ -47,38 +49,6 @@ def input_number():
         except ValueError:
             print("Invalid!Please enter the integer!")
 
-#Define input width, length, radius:
-def input_data_cubes():
-    global cubes_name
-    while True:
-        try:
-            if cubes_name == 'circle' or cubes_name == 'oval':
-                radius = float(input("Please enter a number > 0! Radius:"))
-                if radius > 0:
-                    return radius
-                    break
-                else:
-                    print("Try again!")
-            elif cubes_name == 'square' or cubes_name == 'triangle' or cubes_name == 'pentagon':
-                length = float(input("Please enter a number > 0! Length:"))
-                if length > 0:
-                    return length
-                    break
-                else:
-                    print("Try again!")
-            elif cubes_name == 'rectangle':
-                width = float(input("Please enter a number > 0! Width:"))
-                long = float(input("Please enter a number > 0! Length:"))
-                if width > 0 and long > 0:
-                    return width, long
-                    break
-                else:
-                    print("Try again!")
-            else:
-                break
-        except ValueError:
-            print("Invalid!Please enter the number > 0!")
-
 #Define a function to draw cubes:
 def draw_cubes():
     global cubes_name
@@ -104,11 +74,18 @@ def draw_cubes():
 cubes_name = input_cubes()
 cubes_number = input_number()
 color = input_color()
-radius = input_data_cubes()
-length = input_data_cubes()
-width, long = input_data_cubes()
+if cubes_name == 'circle' or cubes_name == 'oval':
+    radius = float(input("Please enter a number > 0! Radius:"))
+elif cubes_name == 'square' or cubes_name == 'triangle' or cubes_name == 'pentagon':
+    length = float(input("Please enter a number > 0! Length:"))
+elif cubes_name == 'rectangle':
+    width = float(input("Please enter a number > o!Width:"))
+    long = float(input("Please enter a number > 0!Long:"))
+else:
+    print()
 
 turtle.title("Your " + cubes_name)
+turtle.speed(20)
 for i in range(cubes_number):
     draw_cubes()
     turtle.left(360 / cubes_number)
